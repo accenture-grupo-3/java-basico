6ef9dda58c3c54c142f6413987995d6ebe20ba34
Dornelas <julianadornelas71@gmail.com>
2021-06-28 22:25:19 -0300

    manipulando arrays

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,22 +2,42 @@ package application;
 
 import entities.Aluno;
 import entities.Curso;
-import entities.RepositorioAlunosArray;
+//import entities.RepositorioAlunosArray;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		
 		Curso c = new Curso(1, "Programação");
 		Aluno a = new Aluno("João", "123.456", 16, c);
 		Aluno b = new Aluno("josé", "234.567", 16, c);
-		RepositorioAlunosArray arr = new RepositorioAlunosArray();
+		Aluno aa = new Aluno("Kelly", "258.369", 30, c);
+		Aluno aaa = new Aluno("Camila", "147.963", 25, c);
+		ArraysAlunos arr = new ArraysAlunos(3);
+		
+		arr.inserir(aaa);
+		arr.inserir(aa);
+		arr.inserir(a);
+		arr.inserir(b);
+		arr.exibir();
+		System.out.println(arr.procurar("147.963"));
+		arr.remover("123.456");
+		arr.exibir();
+		
+		
+		
+		
+		
+		
+		/*RepositorioAlunosArray arr = new RepositorioAlunosArray();
 		
 		arr.inserirAluno(a);
 		arr.inserirAluno(b);
 		System.out.println(arr.procurarAluno("123.456"));
 		arr.exibeLista();
 		arr.removerAluno("123.456");
-		arr.exibeLista();
+		arr.exibeLista();*/
 	
 	}
 
